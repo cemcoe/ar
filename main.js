7ac9e466d75e9d03b4221383b36985e347036865
cemcoe <chemxcode@gmail.com>
2023-03-12 15:22:47 +0800

    go

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+import { loadGLTF } from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/logo.mind',
+      imageTargetSrc: './assets/targets/logo.mind',
     });
     const { renderer, scene, camera } = mindarThree;
 
@@ -14,9 +16,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
     const plane = new THREE.Mesh(geometry, material);
 
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const model = await loadGLTF('./assets/models/test1.gltf');
+
     // create anchor
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
+    anchor.group.add(model.scene);
 
     // start AR
     await mindarThree.start();
